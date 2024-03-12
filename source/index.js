5584e868ce2c9ec53a16f9c3e044c09d1114c0d0
MykhCher <micher1240gmail.com>
2024-03-12 20:58:39 +0200

    add group name checking

--- source/index.js
+++ source/index.js
@@ -1,1 +1,27 @@
 'use strict';
+
+function checkGroup(array) {
+    const pattern = /^(F[DME]|f[dme])(20\d{2})(-\d+)?$/;
+    for(let i = 0; i < array.length-1; i++) {
+        if (pattern.test(array[i])) {
+            console.log(`Success: ${array[i]}`);
+        } else {
+            console.log(`Error: ${array[i]}`);
+        }
+    }
+}
+
+const arrGroups = [
+    'FE2021-5',
+    'Fe2020-2',
+    'FD2021',
+    'fD2022-1',
+    'FE2020-3',
+    'FD2020/2',
+    'fe2021-3',
+    'FD2020$5',
+    'FE2008-52',
+    'Fm2008-23',
+    'FM2022-1',
+];
+checkGroup(arrGroups);
